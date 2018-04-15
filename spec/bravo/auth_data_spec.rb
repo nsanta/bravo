1fb7ca658a8ee0a39568ff54d12930f03cc7d074
Edu Depetris <edu.depetris.00@gmail.com>
2018-04-15 03:45:23 -0300

    Force update access ticket if it expired

--- spec/bravo/auth_data_spec.rb
+++ spec/bravo/auth_data_spec.rb
@@ -7,7 +7,30 @@ describe 'AuthData' do
 
       Bravo::AuthData.fetch
 
-      Bravo.constants.should include(:TOKEN, :SIGN)
+      Bravo.constants.should include(:TOKEN, :SIGN, :EXPIRE_AT)
+    end
+  end
+
+  describe '.access_ticket_expired?' do
+    it 'returns true when constant EXPIRED_AT does not exists' do
+      Bravo.send(:remove_const, 'EXPIRE_AT')
+      Bravo::AuthData.access_ticket_expired?.should eq true
+    end
+
+    context 'when access ticket has expired' do
+      it 'returns true' do
+        expired_at = Time.now - 54000 # 15 hours
+        Bravo.const_set('EXPIRE_AT', expired_at)
+        Bravo::AuthData.access_ticket_expired?.should eq true
+      end
+    end
+
+    context 'when access ticket has not expired' do
+      it 'returns false' do
+        expire_at = Time.now + 54000 # 15 hours
+        Bravo.const_set('EXPIRE_AT', expire_at)
+        Bravo::AuthData.access_ticket_expired?.should eq false
+      end
     end
   end
 end
